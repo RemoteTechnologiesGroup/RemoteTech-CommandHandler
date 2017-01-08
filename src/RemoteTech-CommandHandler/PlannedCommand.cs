db0018a313dfcd18956c9646fd65ae83b70f278f
Yamori Yuki <yamori.yuki@flamingfly.org>
2017-01-08 12:34:54 +0100

    dropped Command and Condition classes
    moved code from save/load classes into PlannedCommand

--- src/RemoteTech-CommandHandler/PlannedCommand.cs
+++ src/RemoteTech-CommandHandler/PlannedCommand.cs
@@ -8,6 +8,8 @@ namespace RemoteTech.CommandHandler
     class PlannedCommand
     {
         public const string configNodeName = "PlannedCommand";
+        public const string commandNodeName = "Command";
+        public const string conditionNodeName = "Condition";
 
         public readonly ICommand command;
         public readonly ICondition condition;
@@ -15,7 +17,11 @@ namespace RemoteTech.CommandHandler
 
         public PlannedCommand(ConfigNode node)
         {
-
+            ConfigNode subNode = null;
+            if (node.TryGetNode(ProviderManager.providerDataNodeName, ref subNode))
+            {
+                command = LoadCommand(subNode);
+            }
         }
 
         public PlannedCommand(ICommand cmd, ICondition cond, Guid vesselGuid)
@@ -27,9 +33,61 @@ namespace RemoteTech.CommandHandler
 
         public void Save(ConfigNode node)
         {
-            var cmdNode = node.AddNode(Command.commandNodeName);
-            Command.Save(command, cmdNode);
-            //var condNode = node.AddNode(Condition.conditionNodeName);
+            // save command
+            var subNode = node.AddNode(commandNodeName);
+            subNode.AddValue(ProviderManager.providerConfigLabelName, command.ProviderName);
+            subNode = subNode.AddNode(ProviderManager.providerDataNodeName);
+            command.Save(subNode);
+            var subcmd = command.SubCommands;
+            for (var i = 0; i < subcmd.Length; i++)
+            {
+                subNode = node.AddNode(commandNodeName);
+                subcmd[i].Save(subNode);
+            }
+            // save condition
+            
+        }
+
+        private ICommand LoadCommand(ConfigNode node)
+        {
+            string providerName = string.Empty;
+            ConfigNode extNode = null;
+            if (node.TryGetValue(ProviderManager.providerConfigLabelName, ref providerName) && node.TryGetNode(ProviderManager.providerDataNodeName, ref extNode))
+            {
+                var subnodes = node.GetNodes(commandNodeName);
+                var subcmd = new ICommand[subnodes.Length];
+                for (var i = 0; i < subnodes.Length; i++)
+                {
+                    subcmd[i] = LoadCommand(subnodes[i]);
+                }
+                var provider = ProviderManager.FindProvider(providerName);
+                if (provider != null)
+                {
+                    var cmd = provider.LoadCommand(extNode);
+                    for (var i = 0; i < subcmd.Length; i++)
+                    {
+                        cmd.AddSubCommand(subcmd[i]);
+                    }
+                    return cmd;
+                }
+                else
+                {
+                    // log "Unable to find provider with name 'providerName'"
+                }
+            }
+            else
+            {
+                if (providerName == string.Empty)
+                {
+                    // log "Unable to get provider name from config node"
+                }
+                if (extNode == null)
+                {
+                    // log "Unable to get provider data from config node"
+                }
+
+            }
+            return null;
         }
     }
 }
